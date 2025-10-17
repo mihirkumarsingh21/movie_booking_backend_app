e0f4b70ea35079f479d739bc54c9dfce7acd67cb
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-17 17:06:58 +0530

    adding movie functionalty

---
+++ src/controllers/movie.controller.js
@@ -0,0 +1,117 @@
+import { isValidObjectId } from "mongoose";
+import { Movie } from "../models/movie.model.js";
+
+
+
+export const createMovie = async (req, res) => {
+    try {
+        const { name, description, casts, trailerUrl, language, releaseDate, director, releaseStatus } = req.body;
+
+        console.table([name, description, casts, trailerUrl, language, releaseDate, director, releaseStatus]);
+        
+
+        if(!name || !description || !casts || !trailerUrl || !language || !releaseDate || !director || !releaseStatus) return res.status(400).json({
+            success: false,
+            message: "these fields are required."
+        })
+
+       
+        const createdMovie = await Movie.create({
+            name,
+            description,
+            casts,
+            trailerUrl,
+            language,
+            releaseDate,
+            director, 
+            releaseStatus
+        });
+
+        if(!createdMovie) return res.status(400).json({
+            success: false,
+            message: "failed to create to a movie."
+        })
+
+        return res.status(201).json({
+            success: true,
+            message: "new movie created successfully.",
+            newMovie: createdMovie
+        })
+
+
+
+    } catch (error) {
+        console.log(`error while creating new movie: ${error}`);
+        res.status(500).json({
+            succcess: false,
+            errorMessage: `server error something went wrong: ${error}`
+        })
+        return;
+    }
+}
+
+
+export const getMovie = async (req, res) => {
+    try {
+
+        const {movieId} = req.params;
+        console.log(`movieId: ${movieId}`);
+        
+        if(!movieId || !isValidObjectId(movieId)) return res.status(400).json({
+            success: false,
+            message: "movie id are not present in your url Or invalid movie id." 
+        })
+
+        const movie = await Movie.findById(movieId);
+        if(!movie) return res.status(404).json({
+            success: false,
+            message: "movie not found with corresponding id.."
+        })
+
+        return res.status(200).json({
+            success: true,
+            movie: movie
+        })
+
+         
+    } catch (error) {
+        console.log(`error whhile getting single movie: ${error}`);
+        
+        res.status(500).json({
+            success: false,
+            message: `server error something went wrong: ${error}`
+        })
+        return;
+    }
+}
+
+
+export const deleteMovie = async (req, res) => {
+    try {
+        const {movieId} = req.params;
+        if(!movieId || !isValidObjectId(movieId)) return res.status(400).json({
+            success: false,
+            message: "movie id are not present in your url Or invalid movie id." 
+        })
+
+        const deletedMovie = await Movie.findByIdAndDelete(movieId);
+        if(!deletedMovie) return res.status(400).json({
+            succcess: false,
+            message: "failed to delete movie."
+        })
+
+        return res.status(200).json({
+            success: true,
+            message: "movie deleted successfully."
+        })
+
+    } catch (error) {
+        console.log(`error while deleting movie: ${error}`);
+        
+        res.status(500).json({
+            succcess: false,
+            message: `server error something went wrong: ${error}`
+        })
+        return;
+    }
+}
