8b6a081568763455ae4b507cfd4d969e2d505356
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-17 17:06:04 +0530

    import movie routes

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 import { connectToDatabase } from "./src/db/db.js";
 
 
+import movieRoute from "./src/routes/movie.route.js"
+
+
 dotenv.config();
 const app = express();
 
 
 app.use(express.json());
+
+
+app.use("/mba/api/v1/movies", movieRoute);
 
 
 
