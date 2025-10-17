a5a378a88d44980c844cc9fb46deabc297ab86c3
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-17 17:31:36 +0530

    import update route

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { createMovie, getMovie, deleteMovie } from "../controllers/movie.controller.js";
+import { createMovie, getMovie, deleteMovie, updateMovie } from "../controllers/movie.controller.js";
 
 const route = express.Router();
 
 
 route.post("/create-movie", createMovie);
 route.get("/get-movie/:movieId", getMovie);
+route.put("/update-movie/:movieId", updateMovie);
 route.delete("/delete-movie/:movieId", deleteMovie);
+
 
 
 export default route;
